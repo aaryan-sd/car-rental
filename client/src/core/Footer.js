e758e4d1308f7282b14e7a91f89fb3a14418a170
aaryan-sd <91387346+aaryan-sd@users.noreply.github.com>
2023-11-29 23:37:54 +0530

    Update Footer.js

--- client/src/core/Footer.js
+++ client/src/core/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="text-lg">ADS PBL Project</div>
-      <div className="text-l">Team - Aaryan, Siddhant, Shreya, Shweta, Vidya</div>
+      <div className="text-l"></div>
     </footer>
   );
 }
