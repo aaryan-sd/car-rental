c7c7c8563f8b0680a7399c2a6c1bdac635d03385
aaryan-sd <91387346+aaryan-sd@users.noreply.github.com>
2023-11-29 23:49:52 +0530

    Update Footer.js

--- client/src/core/Footer.js
+++ client/src/core/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="text-lg">ADS PBL Project</div>
+      <div className="text-lg">Car Rental MERN stack project</div>
       <div className="text-l"></div>
     </footer>
   );
